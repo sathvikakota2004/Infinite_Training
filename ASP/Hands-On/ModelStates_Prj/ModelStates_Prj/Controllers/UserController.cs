2d84eeae1dbdb0ef687f8983cc3f7fe56c4d0c18
Sathvika  Kota <sathvikak@infinite.com>
2025-08-21 06:37:37 +0000

    training

---
+++ ASP/Hands-On/ModelStates_Prj/ModelStates_Prj/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelStates_Prj.Models;
+
+namespace ModelStates_Prj.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //1. If Validation Succeeds
+        public ActionResult UserStatus()
+        {
+            ViewBag.Status = "Validation Successful";
+            return View();
+        }
+
+        //2. Add user
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(User user)
+        {
+            if (string.IsNullOrEmpty(user.Lname))
+            {
+                ModelState.AddModelError("Lname", "Please Enter Last Name also..");
+            }
+            if (user.age < 21 || user.age > 45)
+            {
+                ModelState.AddModelError("age", "Age only between 21 and 45");
+            }
+            if (string.IsNullOrEmpty(user.Address))
+            {
+                ModelState.AddModelError("Address", "Address is required");
+            }
+
+           
+            if (string.IsNullOrEmpty(user.Password))
+            {
+                ModelState.AddModelError("Password", "Password is required");
+            }
+            
+            else if (user.Password.Length < 6)
+            {
+                ModelState.AddModelError("Password", "Password must be at least 6 characters long");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            else
+            {
+                TempData["lastname"] = user.Lname;
+                TempData["age"] = user.age;
+                TempData["address"] = user.Address;
+                TempData.Keep();
+                return RedirectToAction("UserStatus");
+
+            }
+        }
+    }
+}
---
+++ ASP/Hands-On/ModelStates_Prj/ModelStates_Prj/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ModelStates_Prj.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "FirstName Required")]
+        [Display(Name = "First Name")]
+        [StringLength(20, ErrorMessage = "First Name Cannot be more than 20 characters")]
+        public string Fname { get; set; }
+
+        [DisplayName("Last Name")]
+        public string Lname { get; set; }
+
+        [Display(Name = "Users Age")]
+        [Range(21, 45, ErrorMessage = "Age has to be between 21 and 45 only")]
+        public int age { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Email")]
+        [Display(Name = "Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})$",
+            ErrorMessage = "Invalid Format")]
+        public string email { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Address must be between 5 and 100 characters")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(16, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 16 characters")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
